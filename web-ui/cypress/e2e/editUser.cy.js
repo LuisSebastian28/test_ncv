2c03dfd6e6d319f5a9e83bb82484d3ed1c0fe6f6
LuisSebastian28 <sabestech@gmail.com>
2023-05-10 13:28:57 -0400

    pruebas en rama 8

--- web-ui/cypress/e2e/editUser.cy.js
+++ web-ui/cypress/e2e/editUser.cy.js
@@ -2,7 +2,6 @@ sessionStorage.setItem('Access', "CompleteAccess")
 
 describe('Edit users end to end tests', () => {
   it('Verificar happy path (cambio de numero)', () => {
-    const randomNumber = Math.floor(Math.random() * 100);s
     
   });
 });
